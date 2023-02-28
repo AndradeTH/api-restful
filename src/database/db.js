a107adb75da4f789a1cbc1768c1bc755785cbb02
AndradeTH <deandradeoliveira2@gmail.com>
2023-02-28 20:19:10 -0300

    adicionando model e controller de produtos

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+function connect() {
+
+    mongoose.connect('mongodb+srv://nfdkbsp.mongodb.net/api-restful')
+
+    const db = mongoose.connection
+
+    db.once('opne', () => {
+        console.log('Conected to database!')
+    })
+
+    db.on('error', console.error.bind(console, 'connection error: '))
+}
+
+module.exports = {
+    connect
+}
